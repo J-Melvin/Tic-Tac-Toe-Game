cbfdf05d4f478f2b0e6057f782b19a51c33ac35e
Melvin J <jmelvin1404@gmail.com>
2024-01-28 16:30:33 +0530

    lifted up  the state to App component Show status and Winner

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,42 @@
-import { useState } from 'react';
 import './styles.scss';
+import { useState } from 'react';
 import Board from './components/Board';
+import { calculateWinner } from './winner';
 
 function App() {
-  const [counter, setCounter] = useState(1);
+  const [square, setSquares] = useState(Array(9).fill(null));
+  const [isXNext, setIsNext] = useState(false);
 
-  const onBtnClick = () => {
-    setCounter(currentCounter => {
-      return currentCounter + 1;
+  const winner = calculateWinner(square);
+  const nextPlayer = isXNext ? 'X' : 'O';
+  const statusMessage = winner
+    ? `Winner is ${winner}`
+    : `Next player is ${nextPlayer}`;
+
+  console.log(winner);
+
+  const handleSquareClick = clickedPosition => {
+    if (square[clickedPosition] || winner) {
+      return;
+    }
+
+    setSquares(currentSquares => {
+      return currentSquares.map((squareValue, position) => {
+        if (clickedPosition === position) {
+          return isXNext ? 'X' : 'O';
+        }
+
+        return squareValue;
+      });
     });
+
+    setIsNext(currentIsNext => !currentIsNext);
   };
 
   return (
     <div className="app">
-      <div>
-        <button onClick={onBtnClick}>Click me please</button>
-        <div>{counter}</div>
-        <div>
-          <Board />
-        </div>
-      </div>
+      <h2>{statusMessage}</h2>
+      <Board square={square} handleSquareClick={handleSquareClick} />
     </div>
   );
 }
--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,23 +1,6 @@
-import { useState } from 'react';
 import Square from './Square';
 
-const Board = () => {
-  const [square, setSquares] = useState(Array(9).fill(null));
-
-
-
-  const handleSquareClick = clickedPosition => {
-    setSquares(currentSquares => {
-      return currentSquares.map((squareValue, position) => {
-        if (clickedPosition === position) {
-          return 'X';
-        }
-
-        return squareValue;
-      });
-    });
-  };
-
+const Board = ({ square, handleSquareClick }) => {
   const renderSquare = position => {
     return (
       <Square
