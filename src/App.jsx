0b88d9724768416416190a563cbdf8602562f6b8
Melvin J <jmelvin1404@gmail.com>
2024-01-30 22:17:30 +0530

    Done some Css

--- src/App.jsx
+++ src/App.jsx
@@ -83,6 +83,8 @@ function App() {
       </button>
       <h2>Current game history</h2>
       <History history={history} moveTo={moveTo} currentMove={currentMove} />
+
+      <div className='bg-balls'></div>
     </div>
   );
 }
